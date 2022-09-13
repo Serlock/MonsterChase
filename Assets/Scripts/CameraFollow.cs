65f4e9a9547950f4e4067635fa2f446bab5992b0
serlock <serkanakyuz72@gmail.com>
2022-09-13 17:06:44 +0300

    New changes added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 tempPos;
+    private string PLAYER_TAG = "Player";
+
+    [SerializeField]
+    private float minX, maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag(PLAYER_TAG).transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        tempPos = transform.position;
+        tempPos.x = player.position.x;
+
+        if (tempPos.x < minX)
+            tempPos.x = minX;
+        if (tempPos.x > maxX)
+            tempPos.x = maxX;
+
+        transform.position = tempPos;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,9 +9,12 @@ public class Player : MonoBehaviour
 
     private float movementX;
     public Rigidbody2D myBody;
-    private Animator anim;
+    public Animator anim;
     private SpriteRenderer sr;
     private string WALK_ANIMATION = "Walk";
+    private string GROUND_TAG = "Ground";
+
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -23,18 +26,56 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMoveKeyboard();
+        AnimatePlayer();
+        PlayerJump();
     }
 
     private void PlayerMoveKeyboard()
     {
         movementX = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer()
+    {
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded = true;
+        }
     }
 }
